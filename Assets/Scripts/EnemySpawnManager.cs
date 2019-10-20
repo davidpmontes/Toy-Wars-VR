4c0e9df2a9ddb4149f635ef7a9479b71383d7f9d
Philip <vietwhammies@gmail.com>
2019-10-20 15:06:27 -0700

    Spawn Enemy Manager added with call function for level manager

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -6,23 +6,38 @@ public class EnemySpawnManager : MonoBehaviour
 {
     //public PlayerHealth playerHealth;       // Reference to the player's heatlh.
     public GameObject enemy;                // The enemy prefab to be spawned.
-    public float spawnTimeInterval = 3f;            // How long between each spawn.
-    public Transform spawnPoint;         // An array of the spawn points this enemy can spawn from.
-    
-    // Start is called before the first frame update
-    void Start()
+    public float spawnTimeInterval;            // How long between each spawn.
+    //public Transform spawnPoint;         // An array of the spawn points this enemy can spawn from.
+    public int amountOfEnemies;
+
+    //Start is called before the first frame update
+    /*void Start()
     {
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-        InvokeRepeating("Spawn", spawnTimeInterval, spawnTimeInterval);
+
+        //InvokeRepeating("Spawn", spawnTimeInterval, spawnTimeInterval);
+        //Invoke("Spawn", spawnTimeInterval);
+        //Invoke("Spawn", spawnTimeInterval + 1f);
+        for (int i = 0; i < amountOfEnemies; i++)
+            Invoke("Spawn", spawnTimeInterval + (spawnTimeInterval * i));
+    }*/
+
+    IEnumerator Spawn(GameObject enemy, float startTime, Vector3 spawnPoint, int amountOfEnemies)
+    {
+        // waits initially for an interval of time
+        yield return new WaitForSeconds(startTime);
+
+        // Instantiates an enemy
+        Instantiate(enemy, spawnPoint, Quaternion.identity);
+        
     }
 
-    void Spawn()
+    // lets the Level Manager call the enemy spawn manager
+    public void SpawnEnemies(GameObject enemy, float interval, Vector3 spawnPoint, int amountOfEnemies)
     {
-        // player health
-        // Find a random index between zero and one less than the number of spawn points.
-        
-
-        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+        // For the amount of enemies, spawn them after set amount of time interval
+        for (int i = 0; i < amountOfEnemies; i++)
+            StartCoroutine(Spawn(enemy, interval + (interval * i), spawnPoint, amountOfEnemies));
+            
     }
 }

739f823b6cbae7cb2b7e5c50fe0d7f0632747802
David Montes <david.montes@sjsu.edu>
2019-10-20 18:48:46 -0700

    added spitfire

---
+++ Assets/Spitfire.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Spitfire : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int wpIndex = 0;
+    public float moveSpeed;
+    public float rotateSpeed;
+    [SerializeField] private GameObject tiltable;
+    private Vector3 waypointRandom;
+
+    void Update()
+    {
+        FlyToWaypoint();
+    }
+
+    private void FlyToWaypoint()
+    {
+        var distance = Vector3.Distance(transform.position, waypoints[wpIndex].transform.position + waypointRandom);
+
+        if (distance > 50)
+        {
+            Vector3 targetDir = waypoints[wpIndex].transform.position + waypointRandom - transform.position;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, Time.deltaTime * rotateSpeed, 0.0f);
+            transform.localRotation = Quaternion.LookRotation(newDir);
+
+            transform.Translate(0, 0, Time.deltaTime * moveSpeed);
+        }
+        else
+        {
+            NextWayPoint();
+}
+    }
+
+    private void NextWayPoint()
+    {
+        wpIndex = (++wpIndex) % waypoints.Length;
+        waypointRandom = Random.insideUnitSphere * 30;
+    }
+}
---
+++ Assets/SpitfireSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpitfireSpawner : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Invoke("Spawn", i * 1);
+        }
+    }
+
+    private void Spawn()
+    {
+        var newSpitfire = ObjectPool.Instance.GetFromPoolActiveSetTransform(Pools.Spitfire, transform);
+        newSpitfire.transform.position = transform.position;
+        newSpitfire.transform.SetParent(transform);
+        EnemyManager.Instance.RegisterEnemy(newSpitfire);
+    }
+}

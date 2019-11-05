04d2d49494c03b7b8a29e1575600d41c94881b31
David Montes <david.montes@sjsu.edu>
2019-11-05 09:31:36 -0800

    added tower soldiers and animation controllers

--- Assets/Soldier.cs
+++ Assets/Soldier.cs
@@ -3,24 +3,41 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 
+public enum SOLDIER_TYPE
+{
+    BASE_PERIMETER,
+    TOWER_GUARD
+}
+
 public class Soldier : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
     private UnityAction action;
     private int currIndex;
     private List<Vector3> path = new List<Vector3>();
+    public SOLDIER_TYPE type;
+    private int state = 0;
+    private Animator animator;
 
     private void Awake()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
     {
-        currIndex = 0;
-        path = PathManager.Instance.GetPath(PATHS.PATROL1);
-        navMeshAgent.SetDestination(path[currIndex]);
-        action = PatrolAction;
+        if (type == SOLDIER_TYPE.BASE_PERIMETER)
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            currIndex = 0;
+            path = PathManager.Instance.GetPath(PATHS.PATROL1);
+            navMeshAgent.SetDestination(path[currIndex]);
+            action = BasePatrolAction;
+        }
+        else if (type == SOLDIER_TYPE.TOWER_GUARD)
+        {
+            action = TowerGuardAction;
+        }
     }
 
     void Update()
@@ -28,14 +45,43 @@ public class Soldier : MonoBehaviour
         action();
     }
 
-    private void PatrolAction()
+    private void TowerGuardAction()
     {
-        var distance = Vector3.Distance(transform.position, navMeshAgent.destination);
-
-        if (distance < 1)
+        if (state == 0) //initiate watch
         {
-            currIndex = (currIndex + 1) % path.Count;
-            navMeshAgent.SetDestination(path[currIndex] + new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)));
+            Invoke("Watch", Random.Range(8, 15));
+            state = 1;
         }
+    }
+    
+    private void Watch()
+    {
+        animator.SetTrigger("watch");
+        state = 0;
+    }
+
+    private void BasePatrolAction()
+    {
+        if (state == 0)
+        {
+            Invoke("Walk", Random.Range(0, 2));
+            state = 1;
+        }
+        else if (state == 2)
+        {
+            var distance = Vector3.Distance(transform.position, navMeshAgent.destination);
+
+            if (distance < 1)
+            {
+                currIndex = (currIndex + 1) % path.Count;
+                navMeshAgent.SetDestination(path[currIndex] + new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)));
+            }
+        }
+    }
+
+    private void Walk()
+    {
+        animator.SetTrigger("walk");
+        state = 2;
     }
 }

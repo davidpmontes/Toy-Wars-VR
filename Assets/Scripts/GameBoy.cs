60912f478afb5989d1ff134cfe5764c3974c675e
drumsticktt <drumsticktt@hotmail.com>
2019-11-30 15:12:48 -0800

    room is done, tank physics need fine tuning for ramps

---
+++ Assets/Scripts/GameBoy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoy : MonoBehaviour, IRoomActivate
+{
+    public void Activate()
+    {
+        this.GetComponent<Rigidbody>().AddForce(-25 * this.transform.forward, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour, IRoomActivate
+{
+    [SerializeField] private GameObject arm = default;
+    [SerializeField] private Transform joint = default;
+    private bool rotating = false;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotating)
+        {
+            arm.transform.RotateAround(joint.position, joint.forward, 180 * Time.deltaTime);
+        }
+    }
+
+    public void Activate()
+    {
+        rotating = true;
+        StartCoroutine(StopRotation());
+    }
+
+    private IEnumerator StopRotation()
+    {
+        yield return new WaitForSeconds(0.5f);
+        rotating = false;
+    }
+}
